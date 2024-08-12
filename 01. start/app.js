8decadbb497e0ae83224c6acb8e1768b3f38cc72
SJ.Lee <eagleduk@gmail.com>
2024-08-12 22:48:26 +0900

    01. start > Vanilla JS to VueJS

--- 01. start/app.js
+++ 01. start/app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const ulEl = document.querySelector("ul");
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      inputValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.inputValue);
+      this.inputValue = "";
+    },
+  },
+}).mount("#app");
 
-buttonEl.addEventListener("click", function () {
-  const inputValue = inputEl.value;
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const ulEl = document.querySelector("ul");
 
-  const listItem = document.createElement("li");
-  listItem.textContent = inputValue;
+// buttonEl.addEventListener("click", function () {
+//   const inputValue = inputEl.value;
 
-  ulEl.appendChild(listItem);
-  inputEl.value = "";
-});
+//   const listItem = document.createElement("li");
+//   listItem.textContent = inputValue;
+
+//   ulEl.appendChild(listItem);
+//   inputEl.value = "";
+// });
