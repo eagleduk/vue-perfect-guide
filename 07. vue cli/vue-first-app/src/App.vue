2cb876885c072e9fc42161057679c58defc89d7c
SJ.Lee <eagleduk@gmail.com>
2024-11-18 21:56:19 +0900

    07. vue cli > add App.vue

---
+++ 07. vue cli/vue-first-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "id001",
+          name: "Ronaldo",
+          phone: "000 111 2222",
+          email: "ronaldo@localhost.com",
+        },
+        {
+          id: "id002",
+          name: "Messi",
+          phone: "333 093 3293",
+          email: "messi@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
