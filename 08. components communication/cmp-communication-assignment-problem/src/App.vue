e749e1c4cc68cfc77b5bb6cb2c2af1a9cd95997f
SJ.Lee <eagleduk@gmail.com>
2024-11-21 22:16:01 +0900

    08. components communication > problem

---
+++ 08. components communication/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>User</h1>
+  <user-data @add-user="addUser" />
+  <active-user
+    v-for="user in users"
+    :key="user.id"
+    :name="user.name"
+    :age="user.age"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    addUser(user) {
+      this.users.push({ id: Date.now().toString(), ...user });
+    },
+  },
+};
+</script>
---
+++ 08. components communication/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="addUser">
+    <div>
+      <label for="name"> Name </label>
+      <input id="name" v-model="name" />
+    </div>
+    <div>
+      <label for="age"> Age </label>
+      <input id="age" type="number" v-model="age" />
+    </div>
+
+    <div>
+      <button>Add User</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-user"],
+  data() {
+    return {
+      name: "",
+      age: 0,
+    };
+  },
+  methods: {
+    addUser() {
+      this.$emit("add-user", {
+        name: this.name,
+        age: this.age,
+      });
+    },
+  },
+};
+</script>
