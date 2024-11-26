d90632fe06f3e2e5df2d932240de6d2352657d93
SJ.Lee <eagleduk@gmail.com>
2024-11-26 22:25:17 +0900

    09. components detail > teleport

---
+++ 09. components detail/vue-project/src/components/ErrorAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+dialog {
+  position: fixed;
+  top: 40vh;
+  left: 30%;
+  width: 40%;
+  height: 20vh;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+}
+</style>
--- 09. components detail/vue-project/src/components/TabAComponent.vue
+++ 09. components detail/vue-project/src/components/TabAComponent.vue
@@ -1,6 +1,40 @@
 <template>
   <div>
     <h2>Tab A</h2>
-    <input type="text" />
+    <input type="text" ref="input" />
+    <button @click="checkInput">Add</button>
   </div>
+  <teleport to="body">
+    <ErrorAlert v-if="invalidInput">
+      <h2>Invalid Data</h2>
+      <p>Please, input Data</p>
+      <button @click="confirmError">Close</button>
+    </ErrorAlert>
+  </teleport>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      invalidInput: false,
+    };
+  },
+  methods: {
+    checkInput() {
+      const input = this.$refs.input.value;
+      if (input.trim() === "") {
+        this.invalidInput = true;
+      }
+    },
+    confirmError() {
+      this.invalidInput = false;
+    },
+  },
+};
+</script>
