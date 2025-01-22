447ad0cb807e18ecbcc9609dafd65371cc0a8b20
SJ.Lee <eagleduk@gmail.com>
2025-01-22 21:29:55 +0900

    15. vuex > simple vuex state management

--- 15. vuex/README.md
+++ 15. vuex/README.md
@@ -3,3 +3,5 @@
 ```shell
 npm install vuex@next --save
 ```
+
+- 전역적으로 사용하는 상태 관리를 도와주는 패키지
---
+++ 15. vuex/vue-project/src/components/TheCounter.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>
+    {{ $store.state.counter }}
+  </h1>
+</template>
+
+<script>
+export default {};
+</script>
