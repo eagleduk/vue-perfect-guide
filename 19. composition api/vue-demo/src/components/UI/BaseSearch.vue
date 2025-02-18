d0526b45a7aa5cf5e69b72e45b57c66d9285d1db
SJ.Lee <eagleduk@gmail.com>
2025-02-18 22:16:39 +0900

    19. composition api > self migration to composition api

--- 19. composition api/vue-demo/src/components/UI/BaseSearch.vue
+++ 19. composition api/vue-demo/src/components/UI/BaseSearch.vue
@@ -12,12 +12,20 @@
 <script>
 export default {
   props: ["searchTerm"],
-  emits: ["search"],
-  methods: {
-    search(event) {
-      this.$emit("search", event.target.value);
-    },
+  setup(_, context) {
+    function search(event) {
+      context.emit("search", event.target.value);
+    }
+    return {
+      search,
+    };
   },
+  // emits: ["search"],
+  // methods: {
+  //   search(event) {
+  //     this.$emit("search", event.target.value);
+  //   },
+  // },
 };
 </script>
 
--- 19. composition api/vue-demo/src/components/users/UserItem.vue
+++ 19. composition api/vue-demo/src/components/users/UserItem.vue
@@ -8,12 +8,20 @@
 <script>
 export default {
   props: ["id", "userName"],
-  emits: ["list-projects"],
-  methods: {
-    viewProjects() {
-      this.$emit("list-projects", this.id);
-    },
+  setup(props, context) {
+    function viewProjects() {
+      context.emit("list-projects", props.id);
+    }
+    return {
+      viewProjects,
+    };
   },
+  // emits: ["list-projects"],
+  // methods: {
+  //   viewProjects() {
+  //     this.$emit("list-projects", this.id);
+  //   },
+  // },
 };
 </script>
 
