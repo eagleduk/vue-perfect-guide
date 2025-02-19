79906a14a4768bb00b497ea5b5880268a212373a
SJ.Lee <eagleduk@gmail.com>
2025-02-19 22:40:46 +0900

    20. mixin > search hook end

---
+++ 20. mixin/vue-hook/src/hooks/search.js
@@ -0,0 +1,33 @@
+import { computed, ref, watch } from "vue";
+
+export default function useSearch(items, filterName) {
+  const enteredSearchTerm = ref("");
+  const activeSearchTerm = ref("");
+
+  const availableItems = computed(function () {
+    if (activeSearchTerm.value) {
+      return items.value.filter((item) =>
+        item[filterName].includes(activeSearchTerm.value)
+      );
+    }
+    return items.value;
+  });
+
+  watch(enteredSearchTerm, function (newValue) {
+    setTimeout(() => {
+      if (newValue === enteredSearchTerm.value) {
+        activeSearchTerm.value = newValue;
+      }
+    }, 300);
+  });
+
+  function updateSearch(val) {
+    enteredSearchTerm.value = val;
+  }
+
+  return {
+    enteredSearchTerm,
+    availableItems,
+    updateSearch,
+  };
+}
